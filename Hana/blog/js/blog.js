3578d9e8d2093c87c07a9138ded2be2d1f652f43
decyfeng <decyfeng@gmail.com>
2015-10-18 16:30:58 -0700

    .

--- Hana/blog/js/blog.js
+++ Hana/blog/js/blog.js
@@ -1,5 +1,6 @@
 $(function() {
- 
+	var Parse = require('parse');
+
     Parse.$ = jQuery;
     // Replace this line with the one on your Quickstart Guide Page
 	Parse.initialize("cFdabJuZn4byw5SdCGryu4TAXmtkT1tbyjQDLVkK", "tQPsS5OTVFSDGfsqFw9gsYE2UPHJa5ptEScaNJuw");    
